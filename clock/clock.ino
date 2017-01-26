793cdbbba4437ef50e2113237ceaaa9df5f5c749
Aditya Bawankule <adityabawankule@gmail.com>
2017-01-26 16:33:06 -0600

    Switched to Adafruit.io

--- clock/clock.ino
+++ clock/clock.ino
@@ -4,6 +4,48 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+/************************* WiFi Access Point *********************************/
+#define WLAN_SSID       "network"
+#define WLAN_PASS       "password"
+
+
+/************************* Adafruit.io Setup *********************************/
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  8883
+#define AIO_USERNAME    "user"
+#define AIO_KEY         "key"
+
+WiFiClientSecure client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Subscribe timefeed = Adafruit_MQTT_Subscribe(&mqtt, "time/seconds");
+
+// set timezone offset from UTC
+int timeZone = -4; // UTC - 4 eastern daylight time (nyc)
+int interval = 4; // trigger every X hours
+int hour = 0; // current hour
+
+void timecallback(uint32_t current) {
+
+  // stash previous hour
+  int previous = hour;
+
+  // adjust to local time zone
+  current += (timeZone * 60 * 60);
+
+  // calculate current hour
+  hour = (current / 60 / 60) % 24;
+
+  // only trigger on interval
+  if((hour != previous) && (hour % interval) == 0) {
+    Serial.println("Run your code here");
+  }
+
+}
+
 
 int pinCS = 16; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
 int numberOfHorizontalDisplays = 4;//adjust this to your setup
@@ -30,7 +72,10 @@ void setup() {
     //Serial.print ( "." );
   }
 
-  timeClient.begin();
+   timefeed.setCallback(timecallback);
+  mqtt.subscribe(&timefeed);
+  
+  timeClient.begin();//depreciated soon
 
 
   // put your setup code here, to run once:
